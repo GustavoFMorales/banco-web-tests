50c1b4a27194c052364cd9a1421328f697438331
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-01-28 05:31:43 -0300

    Adicionei teste no login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,5 +7,19 @@ describe('Login', () => {
 
     // Tem uma função chamada contains que verifica se o texto está visivel na tela 
     cy.contains('h4', 'Realizar Transferência').should('be.visible'); // Temos dois parametros, o primeiro é o elemento html que verifica se há um texto h4 na tela, o segundo é o texto que queremos verificar. o .should('be.visible') verifica se o elemento está visivel na tela
+  });
+
+  it('Login com senha inválida deve exibir mensagem de erro', () => {
+    cy.visit('http://localhost:4000');
+    cy.get('#username').click().type('gustavo');
+    cy.get('#senha').click().type('12345'); // senha inválida
+    //cy.get('#login-section > .btn').click();
+    cy.contains('button', 'Entrar').click(); // Outra forma de clicar no botão, procurando pelo texto do botão
+    // Essa forma de clique em botão é a mais recomendada, pois ele descreve qual botão estamos clicando
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.'); // Verifica se a mensagem de erro está visível na tela
+    
+    //cy.get('.toast').should('be.visible').and('contain.text', 'Erro no login. Tente novamente.'); // Verifica se a mensagem de erro está visível na tela
+    // contain.text veirfica se tem uma parte do texto ou uma mensagem toda.
+
   })
-})
+});
