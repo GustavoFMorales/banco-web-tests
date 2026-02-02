6810bca7cd0afab583ab35dd48dd85438927db23
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-02-02 08:25:11 -0300

    Adicionando credenciais de acesso

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,17 +3,26 @@ describe('Login', () => {
     cy.visit('http://localhost:4000')
   });
   it('Login com dados váldos devem permitir entrada no sistema', () => {
-    cy.get('#username').click().type('gustavo');
-    cy.get('#senha').click().type('123456');
+    cy.fixture('credenciais').then(credenciais=> {
+       cy.get('#username').click().type(credenciais.valida.usuario);
+       cy.get('#senha').click().type(credenciais.valida.senha);
+    })
     cy.get('#login-section > .btn').click();
 
     // Tem uma função chamada contains que verifica se o texto está visivel na tela 
-    cy.contains('h4', 'Realizar Transferência').should('be.visible'); // Temos dois parametros, o primeiro é o elemento html que verifica se há um texto h4 na tela, o segundo é o texto que queremos verificar. o .should('be.visible') verifica se o elemento está visivel na tela
+    cy.wait(1000); // Espera 1 segundo para garantir que a página carregou
+    cy.contains('h4', 'Realizar Transferência').should('be.visible'); 
+    
+    // Temos dois parametros, o primeiro é o elemento html que verifica se há um texto h4 na tela, o segundo é o texto que queremos verificar. o .should('be.visible') verifica se o elemento está visivel na tela
   });
 
   it('Login com senha inválida deve exibir mensagem de erro', () => {
-    cy.get('#username').click().type('gustavo');
-    cy.get('#senha').click().type('12345'); // senha inválida
+
+    cy.fixture('credenciais').then(credenciais => {
+       cy.get('#username').click().type(credenciais.invalida.usuario);
+       cy.get('#senha').click().type(credenciais.invalida.senha);
+    })
+   
     //cy.get('#login-section > .btn').click();
     cy.contains('button', 'Entrar').click(); // Outra forma de clicar no botão, procurando pelo texto do botão
     // Essa forma de clique em botão é a mais recomendada, pois ele descreve qual botão estamos clicando
