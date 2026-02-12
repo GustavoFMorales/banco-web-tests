adc2b3ddf285b3941e3b9e6f572dd9cdea04c031
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-02-11 21:28:37 -0300

    Adicionando o script de teste de transferencias

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+    cy.visit('/');
+    cy.fixture('credenciais').then(credenciais => {
+         cy.get('#username')
+            .click()
+            .type(credenciais.valida.usuario);
+         cy.get('#senha')
+            .click()
+            .type(credenciais.valida.senha);
+    });
+    cy.get('#login-section > .btn')
+        .click();
+    cy.get(':nth-child(1) > .col > h4')
+        .should('be.visible');
+});
+
+Cypress.Commands.add("AdicionaConta",(elementoHTML, apelido, dadoClique) => {
+    cy.get(elementoHTML).parent().as(apelido);
+    cy.get(`@${apelido}`).click();
+    cy.get(`@${apelido}`).contains(dadoClique, { matchCase: false }).click(); // O matchCase: false é para ignorar a diferença entre maiúsculas e minúsculas na hora de procurar o texto. Assim, ele vai encontrar o texto mesmo que esteja em maiúsculas ou minúsculas.
+});
